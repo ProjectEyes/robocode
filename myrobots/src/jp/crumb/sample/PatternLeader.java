8226dc27de4c0098aca25e7f132296ffe1e3019d
Hiroaki Kubota <cat.s.panch@gmail.com>
2012-12-13 18:38:14 +0900

    react pattern

---
+++ myrobots/src/jp/crumb/sample/PatternLeader.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.crumb.sample;
+
+import jp.crumb.ace.AceContext;
+import jp.crumb.ace.AceRobot;
+
+
+
+/**
+ *
+ * @author crumb
+ */
+public class AceLeader extends AceRobot<AceContext> {
+
+}
